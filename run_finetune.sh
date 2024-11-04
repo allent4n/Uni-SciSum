ffd08f5dcefd43f2c00b955538c18380531f3f23
AllenTAN <78404109+allent4n@users.noreply.github.com>
2024-11-05 00:22:47 +0800

    Update run_finetune.sh

--- run_finetune.sh
+++ run_finetune.sh
@@ -5,7 +5,7 @@ unzip data.zip
 rm data.zip
 
 wget -c -O checkpoint_finetune.pth https://liveln-my.sharepoint.com/:u:/g/personal/allentan_ln_hk/EULBYEXybNFDpc3gOZD1NQABCzWEjR_nlD9Rg-7USuAcfQ?download=1
-mv checkpoint_cell_finetune.pth ckpts/finetune_ckpts/
+mv checkpoint_finetune.pth ckpts/finetune_ckpts/
 
 
 cd train/finetune
